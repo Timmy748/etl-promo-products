b7730263a09b8de1aec8675ec552d64ec8ccbdec
Timmy Chen <timmychen748@gmail.com>
2025-11-09 15:02:00 -0300

    refact(AsyncRequests): Implementado o async with para interface AsyncRequests.

---
+++ src/services/interfaces/AsyncRequestsInterface.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+from typing import Protocol, TypedDict, final
+
+
+class Response(TypedDict):
+    """Dicionario que representa a resposta da requisição."""
+
+    url: str
+    text: str
+    status_code: int
+
+
+class AsyncRequestsInterface(Protocol):
+    """Interface para."""
+
+    @abstractmethod
+    async def get(self, url: str) -> Response: ...
+
+    @abstractmethod
+    async def aclose(self) -> None: ...
+
+    @final
+    async def __aenter__(self) -> "AsyncRequestsInterface":
+        """Retorna uma AsyncRequestsInterface quando usado async with."""
+        return self
+
+    @final
+    async def __aclose__(self) -> None:
+        """Fecha o AsyncRequestsInterface quando sair do bloco async with."""
+        await self.aclose()
